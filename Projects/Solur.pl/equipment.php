6afb3239b86c254dee5db976293f33faf346ffef
Ivan Biziuk <nefaslex@gmail.com>
2016-09-20 11:03:03 +0300

    add my project Solur.pl

---
+++ Projects/Solur.pl/equipment.php
@@ -0,0 +1,246 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name ="description" content="Solur">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="canonical" href="http://solur.pl">
+	<link href="http://fonts.googleapis.com/css?family=Scada:700,400&amp;subset=latin,cyrillic" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Scada:700,400&amp;subset=latin,cyrillic" rel="stylesheet" type="text/css">
+	<link rel ="stylesheet" type="text/css" href="css/style.css">
+	<link rel ="stylesheet" type="text/css" href="css/button.css">
+	<link rel='stylesheet' type='text/css' href='css/bootstrap.min.css'>
+	<link rel='stylesheet' type='text/css' href='css/animate.css'>
+	<link rel ="stylesheet" type="text/css" href="css/mobile.css">
+	<link rel="icon" href="http://solur.pl/images/favicon.ico">
+	<link rel="shortcut icon" href="http://solur.pl/images/favicon.ico">
+	<script type="text/javascript" src="js/jquery-2.2.0.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+	<script type="text/javascript" src="js/animate.js"></script>
+	<script type="text/javascript" src="js/scroll.js"></script>
+	<title>solur</title>
+</head>
+
+<body>
+<header>
+	<div class="top-line-head"></div>
+	<div id = main-head>
+	<a class="header-logo" href="index.php"><img src="images/logo.png" alt="Logo"></a>
+	<div class="header-right-block">
+		<div class="top-section">
+			<a class ="list" href="#anchor1">send request</a>
+			<div class="lang">
+				<a class ="sel" href="index.php">ENG</a>
+				<a href="#">POL</a></div>
+		</div>
+		<nav class = "header-menu">
+			<ul>
+				<li class="current">Menu</li>
+				<li><a class ="button medium gray" href="company.php">COMPANY</a></li>
+				<li><a class ="button medium gray" href="production.php">PRODUCTION</a></li>
+				<li class="menu-sel"><a class ="button medium gray" href="equipment.php">EQUIPMENT</a></li>
+				<li class="products"><a class ="button medium gray" href="products.php">PRODUCTS</a>
+					<ul class="dropdown">
+						<li><a href="aluminum-profile-for-general-construction.php">Aluminum profile for general construction</a></li>
+						<li><a href="aluminum-profile-for-suspended-ceilings.php">Aluminum profile for suspended ceilings</a></li>
+						<li><a href="aluminum-profile-for-trade-fairs-and-promotional-designs.php">Aluminum profile for trade fairs and promotional designs</a></li>
+						<li><a href="aluminum-sills.php">Aluminum sills</a></li>
+						<li><a href="aluminum-profiles-for-facades.php">Aluminum profiles for facades</a></li>
+						<li><a href="finishing-aluminum-extrusions-and-moldings.php">Finishing aluminum extrusions and moldings</a></li>
+						<li><a href="transport-aluminum-profile.php">Transport aluminum profile</a></li>
+						<li><a href="aluminum-profile-for-mosquito-nets.php">Aluminum profile for mosquito nets</a></li>
+						<li><a href="aluminum-profile-for-mud-treatment-systems.php">Aluminum profile for mud treatment systems</a></li>
+						<li><a href="machine-aluminum-profile.php">Machine aluminum profile</a></li>
+						<li><a href="electro-aluminum-profiles.php">Electro aluminum profiles</a></li>
+						<li><a href="aluminum-profiles-according-to-customer-drawings.php">Aluminum profiles according to customer drawings</a></li>
+						</ul>
+					</li>
+				<!--<li><a class ="button medium gray" href="gallery.php">GALLERY</a></li>-->
+				<li><a class ="button medium gray" href="contacts.php">CONTACTS</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+	<div id="wrapper">
+		<div id="main">
+		    <div class="section-slider">
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+					</ol>
+					<div class="carousel-inner">
+					<div class="texture"></div>
+						<div class="item active">
+							<img src="images/slider/1.jpg" width="100%" alt="Slider">
+							<div class="carousel-caption"></div>
+						</div>
+						<div class="item">
+							<img src="images/slider/2.jpg" width="100%" alt="Slider">
+							<div class="carousel-caption"></div>
+						</div>
+						<div class="item">
+							<img src="images/slider/3.jpg" width="100%" alt="Slider">
+							<div class="carousel-caption"></div>
+						</div>
+						<div class="item">
+							<img src="images/slider/4.jpg" width="100%" alt="Slider">
+						    <div class="carousel-caption"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="breadcrumb"><a href="index.php">Solur</a> >> <a href="equipment.php">Equipment</a></div>
+			<div class="main-content">
+				<!--<div class="section-one">
+				<div class="top-cont-line">
+					<h2>Activity</h2>
+				</div>
+				<section class="text-block">
+				<p class = "txt">Within the period of more than 10 years Ukrainian Aluminium Company “Solur” has become one of the leaders at the Ukrainian market of manufacturers of aluminium alloys and extrusion of aluminium profiles.</p>
+                <p class = "txt">Today, due to the smart investing policy, the Company continues its development and increases production capacities.</p>
+                <p class = "txt">The total monthly production capacity of the Company goes up to 600 tons of aluminium profiles.</p>
+                <p class = "txt">The equipment used by our Company is the most up-to-date one in the field of the aluminium profile extrusion. Fully automated production operations ensure excellent surface treatment and perfect geometry of the extruded profiles. Technological innovations in the fields of cutting, cooling and application of coating enable mitigation of the human factor during production and ensure compliance of the products with the international quality standards.</p>
+                <p class = "txt">The own casting complex including efficient homogenization system guarantees stable and continuous supply of aluminium billets for extrusion.</p>
+				</section>
+				</div>-->
+				<div class="section-two">
+				<div class="top-cont-line">
+				<h2>Equipment</h2>
+				</div>
+                <section class="text-block-middle">
+					<ul>
+					<li>SOLUR ALUMINIUM Sp. z o.o. is up-to-date, dynamically developing company working in the field of aluminium recycling and manufacturing of top quality and high-tech products from aluminium alloys.</li>
+					<li>For production of aluminium alloys the complete complex for sorting, preparation and recycling of aluminium scrap has been installed on the territory of the factory.</li>
+					<li>The advanced technologies and equipment supplied by the best international producers are used for manufacturing of high-quality products.</li>
+				    <li><img src="images/litie.jpg"><span>Melting and casting line</span> includes:
+                                                                                             melting furnace with controlled percentage composition of oxygen, high-efficiency regenerative burners and electromagnetic pump for melt;
+                                                                                             tilting casting furnace with high-efficiency regenerative burners, system for aluminium alloys refining and degassing by Argon;
+                                                                                             filtration system for molten aluminium.
+                                                                                             We offer to our customers the complete assortment of the aluminium profiles from the 6000 alloy series. </li>
+				    <li><img src="images/preiews_lite.jpg"><span>Casting line for semi-continuous casting of aluminium billets</span> includes:
+                            system for billet casting with the AirSlip technology, the casting mould with 26 moulds with the diameter of 152, 175 or 178 mm;
+                            melt purification system; filtration system.</li>
+
+				    <li><img src="images/gomogen.jpg"><span>Equipment for homogenizing of aluminium billets</span> includes:
+                                                                                                                          homogenizing furnace;
+                                                                                                                          cooling unit;
+                                                                                                                          charging machine.</li>
+					</ul>
+                </section>
+				</div>
+				<!--<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+								OPEN VIDEO
+							</a>
+						</h3>
+					</div>
+					<div id="collapseOne" class="panel-collapse collapse">
+						<div class="panel-body">
+							<iframe src="https://player.vimeo.com/video/50969212" width="100%" height="575" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+							<p><a href="https://vimeo.com/50969212">SOLUR SPoLKA zo.o</a> from <a href="https://vimeo.com/solur">Solur</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+						</div>
+					</div>
+				</div>-->
+				<div class="section-three">
+					<div class="section-three-bk">
+				    <div class="top-cont-line">
+					<h2>certified laboratory</h2>
+				</div>
+				<section class="text-block-bottom">
+					<ul>
+					<li>The company is equipped with the in-house certified laboratory where the final products are under control and testing continuously. Control of the alloy chemical composition is performed with the use of high quality spectrometers.</li>
+					<li>The operating staff had taken appropriate trainings including those held abroad. </li>
+					<li>The quality control system of the company is functioning according to ISO9001:2008 standard (DSTU ISO 9001:2009).</li>
+					<li>The production capacity of the company makes 45000 tons of products per year including 7200 tons of aluminium profile.</li>
+					</ul>
+				</section>
+				</div>
+					</div>-->
+					<!--<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+									OPEN VIDEO
+								</a>
+							</h3>
+						</div>
+						<div id="collapseTwo" class="panel-collapse collapse">
+							<div class="panel-body">
+								<iframe width="100%" height="576" src="https://www.youtube.com/embed/yZrFwY5Ysto" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+					</div>-->
+				<!--<section class="text-block-last">
+					<h3>Competitiveness of our customers is our success.</h3>
+				</section>-->
+			</div>
+			</div>
+		</div>
+		<footer>
+			<div class="footer-top-section">
+				<div class="footer-left"></div>
+				<div class="footer-feedback">
+					<form action="#cont_form" method="post" id="cont_form">
+						<div class="header"><span id="anchor1">Send request</span></div>
+						<div class="input">
+							<input type="text" id="name" name="name" placeholder="Your name">
+						</div>
+						<div class="input">
+							<input type="text" id="email" name="email" placeholder="Your e-mail">
+						</div>
+						<div class="input">
+							<textarea id="message" name="message" cols="30" rows="10" placeholder="Your message"></textarea>
+						</div>
+						<div class="submit">
+                                <input type="submit" name="send" value="Send">
+						</div>
+						<div id="error" style="display: none;">ERROR</div>
+						<div id="success" style="display: none;">
+							<h3>Your message has been successfully sent</h3>
+						</div>
+					</form>
+				</div>
+				<div class="footer-right"></div>
+			</div>
+			
+			<div id="up-top">UP</div>
+			<div class="footer-copyright">Copyright © 2013-2016 SOLUR ALUMINIUM Sp. z o.o. All rights reserved. Made by <a href="http://blogstarter.ru" target="_blank">Blogstarter Studio</a></div>
+		</footer>
+<?php
+if(isset($_POST['send'])) {
+	$name = htmlspecialchars($_POST["name"]);
+	$email = htmlspecialchars($_POST['email']);
+
+	if (empty($_POST['name']))
+		output_err();
+
+	// если неправильно заполнено поле email - показываем ошибку
+	if (!preg_match("/[0-9a-z_]+@[0-9a-z_^\.]+\.[a-z]{2,3}/i", $_POST['email']))
+		output_err();
+	$to = ('s.pawlowski@solur.pl, b.w@solur.pl, a.popowicz@solur.pl');
+	$subject = 'Message from site';
+	$message = $name . ' ' . $email . "\r\n" . htmlspecialchars($_POST['message']);
+	$headers = 'From: solur' . "\r\n" . 'Content-type: text/plain; charset=\"utf-8\"';
+	$mail = mail($to, $subject, $message, $headers);
+	if (TRUE === $mail) echo "<script>$(function(){
+                                   $(\"#success\").css(\"display\", \"block\");
+                                   })
+                                   </script>";
+}
+function output_err(){
+	echo "<script>
+         $(function(){
+         $(\"#error\").css(\"display\", \"block\");
+         })
+         </script>";
+	exit();
+}
+?>
+</body>
+</html>
---
+++ Projects/Solur.pl/pol/production.php
@@ -0,0 +1,244 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name ="description" content="Solur">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="canonical" href="http://solur.pl">
+	<link href="http://fonts.googleapis.com/css?family=Scada:700,400&amp;subset=latin,cyrillic" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Scada:700,400&amp;subset=latin,cyrillic" rel="stylesheet" type="text/css">
+	<link rel ="stylesheet" type="text/css" href="../css/style.css">
+	<link rel ="stylesheet" type="text/css" href="../css/button.css">
+	<link rel='stylesheet' type='text/css' href='../css/bootstrap.min.css'>
+	<link rel='stylesheet' type='text/css' href='../css/animate.css'>
+	<link rel ="stylesheet" type="text/css" href="../css/mobile.css">
+	<link rel="icon" href="http://solur.pl/images/favicon.ico">
+	<link rel="shortcut icon" href="http://solur.pl/images/favicon.ico">
+	<script type="text/javascript" src="../js/jquery-2.2.0.min.js"></script>
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../js/script.js"></script>
+	<script type="text/javascript" src="../js/animate.js"></script>
+	<script type="text/javascript" src="../js/scroll.js"></script>
+	<title>solur</title>
+</head>
+
+<body>
+<header>
+	<div class="top-line-head"></div>
+	<div id = main-head>
+	<a class="header-logo" href="index.php"><img src="../images/logo.png" alt="Logo"></a>
+	<div class="header-right-block">
+		<div class="top-section">
+			<a class ="list" href="#anchor1">send request</a>
+			<div class="lang">
+				<a class ="sel" href="index.html">ENG</a>
+				<a href="../index.html">POL</a></div>
+		</div>
+		<nav class = "header-menu">
+			<ul>
+				<li class="current">Menu</li>
+				<li><a class ="button medium gray" href="company.php">COMPANY</a></li>
+				<li class="menu-sel"><a class ="button medium gray" href="production.php">PRODUCTION</a></li>
+				<li><a class ="button medium gray" href="equipment.php">EQUIPMENT</a></li>
+				<li class="products"><a class ="button medium gray" href="products.php">PRODUCTS</a>
+					<ul class="dropdown">
+						<li><a href="aluminum-profile-for-general-construction.php">Aluminum profile for general construction</a></li>
+						<li><a href="aluminum-profile-for-suspended-ceilings.php">Aluminum profile for suspended ceilings</a></li>
+						<li><a href="aluminum-profile-for-trade-fairs-and-promotional-designs.php">Aluminum profile for trade fairs and promotional designs</a></li>
+						<li><a href="aluminum-sills.php">Aluminum sills</a></li>
+						<li><a href="aluminum-profiles-for-facades.php">Aluminum profiles for facades</a></li>
+						<li><a href="finishing-aluminum-extrusions-and-moldings.php">Finishing aluminum extrusions and moldings</a></li>
+						<li><a href="transport-aluminum-profile.php">Transport aluminum profile</a></li>
+						<li><a href="aluminum-profile-for-mosquito-nets.php">Aluminum profile for mosquito nets</a></li>
+						<li><a href="aluminum-profile-for-mud-treatment-systems.php">Aluminum profile for mud treatment systems</a></li>
+						<li><a href="machine-aluminum-profile.php">Machine aluminum profile</a></li>
+						<li><a href="electro-aluminum-profiles.php">Electro aluminum profiles</a></li>
+						<li><a href="aluminum-profiles-according-to-customer-drawings.php">Aluminum profiles according to customer drawings</a></li>
+						</ul>
+					</li>
+				<li><a class ="button medium gray" href="gallery.php">GALLERY</a></li>
+				<li><a class ="button medium gray" href="contacts.php">CONTACTS</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+	<div id="wrapper">
+		<div id="main">
+		    <div class="section-slider">
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+						<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+					</ol>
+					<div class="carousel-inner">
+					<div class="texture"></div>
+						<div class="item active">
+							<img src="../images/slider/1.jpg" width="100%" alt="Slider">
+							<div class="carousel-caption"></div>
+						</div>
+						<div class="item">
+							<img src="../images/slider/2.jpg" width="100%" alt="Slider">
+							<div class="carousel-caption"></div>
+						</div>
+						<div class="item">
+							<img src="../images/slider/3.jpg" width="100%" alt="Slider">
+							<div class="carousel-caption"></div>
+						</div>
+						<div class="item">
+							<img src="../images/slider/4.jpg" width="100%" alt="Slider">
+						    <div class="carousel-caption"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="breadcrumb"><a href="index.php">Solur</a> >> <a href="production.php">Production</a></div>
+			<div class="main-content">
+				<!--<div class="section-one">
+				<div class="top-cont-line">
+					<h2>Activity</h2>
+				</div>
+				<section class="text-block">
+				<p class = "txt">Within the period of more than 10 years Ukrainian Aluminium Company “Solur” has become one of the leaders at the Ukrainian market of manufacturers of aluminium alloys and extrusion of aluminium profiles.</p>
+                <p class = "txt">Today, due to the smart investing policy, the Company continues its development and increases production capacities.</p>
+                <p class = "txt">The total monthly production capacity of the Company goes up to 600 tons of aluminium profiles.</p>
+                <p class = "txt">The equipment used by our Company is the most up-to-date one in the field of the aluminium profile extrusion. Fully automated production operations ensure excellent surface treatment and perfect geometry of the extruded profiles. Technological innovations in the fields of cutting, cooling and application of coating enable mitigation of the human factor during production and ensure compliance of the products with the international quality standards.</p>
+                <p class = "txt">The own casting complex including efficient homogenization system guarantees stable and continuous supply of aluminium billets for extrusion.</p>
+				</section>
+				</div>-->
+				<div class="section-two">
+				<div class="top-cont-line">
+				<h2>PRODUCTION</h2>
+				</div>
+                <section class="text-block-middle">
+					<ul>
+				<li><img src="../images/litie.jpg"><span>Casting line.</span> Our own casting equipment ensures stable and continuous supply of the extrusion production with high-quality raw material complying both with State Standard Specification 4784-97, and international Standard DIN 573-3. The melting furnace with the melt stirring, inert-gas refining, metal filtering through a ceramic foam filter as well as the homogenization furnace makes it possible to be sure in the homogeneity of the aluminium billet.
+                                                   We offer to our customers the complete assortment of the aluminium profiles from the 6000 alloy series.</li>
+				<li><img src="../images/extrusion-line.jpg"><span>Extrusion line.</span> Includes the high-speed short-stroke extrusion press enabling fully automated production operations featuring the following technical specifications:
+                    Press capacity - 16/17.4 Mn
+                    Maximum length of the semifinished section (billet) – 1000 mm
+                    Billet diameter – 178 mm</li>
+				<li><img src="../images/okolo.jpg"><span>Extrusion Handling line.</span> Includes:furnace for billet taper heating; press shear; adjustable line; ageing furnace for heat treatment of the aluminium profile.</li>
+				<li><img src="../images/paint.jpg"><span>Powder coating line</span> Highly-productive and energy-efficient line for vertical application of the polymeric powder coating is unique for Ukraine. The line is equipped with the chrome-free technology for profile pre-painting preparation that enables powder coating of profiles with the length up to 7000 mm. Option of automatic change of the colorant within 20 minutes. Thickness variation of the coating at the length of 7000 mm does not exceed +/-5 micron. The powder coated profile complies with the requirements of the national standards.</li>
+				<li><img src="../images/pack.jpg"><span>Packaging line.</span> The packing machines allow automatic packing of the powder coated and uncoated profiles with the use of a paper or plastic tape. Different packaging methods of the finished products ensuring their preservation by transportation and in storage are applicable. We are happy to be able to satisfy the wishes of our customers!</li>
+				<li><img src="../images/komplex.jpg"><span>The complex for the hard tooling adjustment and repair.</span> Includes special equipment manufactured by the best foreign companies.</li>
+					</ul>
+                </section>
+				</div>
+				<!--<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+								OPEN VIDEO
+							</a>
+						</h3>
+					</div>
+					<div id="collapseOne" class="panel-collapse collapse">
+						<div class="panel-body">
+							<iframe src="https://player.vimeo.com/video/50969212" width="100%" height="575" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+							<p><a href="https://vimeo.com/50969212">SOLUR SPóŁKA zo.o</a> from <a href="https://vimeo.com/solur">Solur</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+						</div>
+					</div>
+				</div>-->
+				<!--<div class="section-three">
+					<div class="section-three-bk">
+				<div class="top-cont-line">
+					<h2>Goals</h2>
+				</div>
+				<section class="text-block-bottom">
+					<ul>
+					<li>Our goal is to become the leader among aluminium profile extruders.</li>
+					<li>The production policy of our company is focused on achievement of the international standards of productivity and quality of the products.</li>
+					<li>Due to modern managing system and staff professionalism, multi-stage quality control system at all production phases we ensure our customers with precision and repeatable accuracy of the technical specifications, reliability and long life-time of the final products together with competitive prices.</li>
+					</ul>
+				</section>
+				</div>
+					</div>-->
+					<!--<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+									OPEN VIDEO
+								</a>
+							</h3>
+						</div>
+						<div id="collapseTwo" class="panel-collapse collapse">
+							<div class="panel-body">
+								<iframe width="100%" height="576" src="https://www.youtube.com/embed/yZrFwY5Ysto" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+					</div>-->
+				<!--<section class="text-block-last">
+					<h3>Competitiveness of our customers is our success.</h3>
+				</section>-->
+			</div>
+			</div>
+		</div>
+		<footer>
+			<div class="footer-top-section">
+				<div class="footer-left"></div>
+				<div class="footer-feedback">
+					<form action="#cont_form" method="post" id="cont_form">
+						<div class="header"><span id="anchor1">Send request</span></div>
+						<div class="input">
+							<input type="text" id="name" name="name" placeholder="Your name">
+						</div>
+						<div class="input">
+							<input type="text" id="email" name="email" placeholder="Your e-mail">
+						</div>
+						<div class="input">
+							<textarea id="message" name="message" cols="30" rows="10" placeholder="Your message"></textarea>
+						</div>
+						<div class="submit">
+                                <input type="submit" name="send" value="Send">
+						</div>
+						<div id="error" style="display: none;">ERROR</div>
+						<div id="success" style="display: none;">
+							<h3>Your message has been successfully sent</h3>
+						</div>
+					</form>
+				</div>
+				<div class="footer-right"></div>
+			</div>
+			<div class="footer-copyright"></div>
+			<div id="up-top">UP</div>
+		</footer>
+<?php
+if(isset($_POST['send'])){
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
+
+           if (empty($_POST['name']))
+               output_err();
+          // если неправильно заполнено поле email - показываем ошибку 1
+          if(!preg_match("/[0-9a-z_]+@[0-9a-z_^\.]+\.[a-z]{2,3}/i", $_POST['email']))
+               output_err();
+          // если не заполнено поле "Сообщение" - показываем ошибку 2
+          if(empty($_POST['mess']))
+               output_err();
+
+$to = 'nefaslex@gmail.com';
+$subject = 'Message';
+$message = $name.' '.$email."\r\n".htmlspecialchars($_POST['message']);
+$headers = 'From: solur_sales@ukr.net' . "\r\n".'Content-type: text/plain; charset=\"utf-8\"';
+$mail= mail($to, $subject, $message, $headers);
+if(TRUE === $mail){ echo "<script>
+                                   $(function(){
+                                   $(\"#success\").css(\"display\", \"block\");
+                                   })
+                                   </script>";
+                    }
+    else echo "<script>alert(\"ERROR SENDING\");<//script>";
+}
+function output_err(){
+   echo "<script>
+         $(function(){
+         $(\"#error\").css(\"display\", \"block\");
+         })
+         </script>";
+   exit();
+}
+?>
+</body>
+</html>
